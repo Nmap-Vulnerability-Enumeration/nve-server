8880c215ddea4c09353bfa86635958d4678843fe
samigirma1 <sgz4>
2021-04-20 17:57:22 -0400

    Updated Devices and Vulnerabilities class to match with strings

--- src/device.py
+++ src/device.py
@@ -1,9 +1,12 @@
 import json
+import utils
+
 
 class Device:
     '''
     Device class represents the devices discovered by the scanner.
     '''
+
     def __init__(self,
                  ip: dict,
                  mac: str = None,
@@ -25,22 +28,24 @@ class Device:
         self.tcp_ports = tcp_ports
         self.uptime = uptime
         self.vendor = vendor
-        self.vulnerabilities = vulnerabilities
+        self.vulns = vulnerabilities
 
     @staticmethod
     def from_nmap(_dict, discovery_ip):
-        return Device(
-                    Device._get_ips(_dict["addresses"], discovery_ip),
-                    Device._get_mac(_dict["addresses"]),
-                    Device._get_name(_dict["hostnames"]),
-                    Device._get_os(_dict["osmatch"]),
-                    _dict["status"],
-                    _dict["portused"],
-                    _dict["tcp"] if "tcp" in _dict else None,
-                    _dict["uptime"]["seconds"] if "uptime" in _dict else None,
-                    Device._get_vendor(_dict["vendor"]) if "vendor" in _dict else None
-                )
-    
+        ip = Device._get_ips(_dict["addresses"], discovery_ip)
+        mac = Device._get_mac(_dict["addresses"])
+        name = Device._get_name(_dict["hostnames"])
+        os = Device._get_os(_dict["osmatch"])
+        tcp_ports = {int(key): val for (key, val)
+                     in _dict["tcp"].items()} if "tcp" in _dict else dict()
+        vendor = Device._get_vendor(
+            _dict["vendor"]) if "vendor" in _dict else None
+
+        return Device(ip, mac, name, os, _dict["status"],
+                      _dict["portused"], tcp_ports,
+                      _dict["uptime"]["seconds"] if "uptime" in _dict else None,
+                      vendor)
+
     @staticmethod
     def _get_ips(addresses, discovery_ip):
         if addresses == None or len(addresses) == 0:
@@ -58,14 +63,14 @@ class Device:
     def _get_mac(addresses):
         if addresses == None or len(addresses) == 0:
             return None
-        
+
         return addresses["mac"] if "mac" in addresses else None
 
     @staticmethod
     def _get_name(hostnames):
         if len(hostnames) == 0:
             return None
-        
+
         return hostnames[0]["name"]
 
     @staticmethod
@@ -74,18 +79,18 @@ class Device:
             return None
 
         estimate = osmatch[0]
-        
+
         for os in osmatch:
             if int(estimate["accuracy"]) < int(os["accuracy"]):
                 estimate = os
-        
+
         ret = dict()
         ret["name"] = estimate["name"]
         ret["accuracy"] = int(estimate["accuracy"])
         ret["osclass"] = Device._get_os_details(estimate["osclass"])
 
         return ret
-    
+
     @staticmethod
     def _get_os_details(osclass):
         max_accuracy = -1
@@ -96,10 +101,10 @@ class Device:
                 classes[accuracy] = [_class]
             else:
                 classes[accuracy].append(_class)
-            
+
             if max_accuracy < accuracy:
                 max_accuracy = accuracy
-        
+
         return classes[max_accuracy]
 
     @staticmethod
@@ -108,6 +113,75 @@ class Device:
             return None
         else:
             return vendor
+
+    def get_all_cpes(self):
+        container = dict()
+
+        # get cpe from tcp ports
+        for port in self.tcp_ports:
+            item = self.tcp_ports[port]
+            cpe = item["cpe"]
+            # add version if missing in CPE
+            if utils.get_version(cpe) == None and "version" in item and len(item["version"]) > 0:
+                ver_list = item["version"].split(" ")
+                # only take the first str
+                if ver_list[0].find(".X") != -1:
+                    version = ver_list[0][:ver_list[0].find(".X")]
+                else:
+                    version = ver_list[0]
+                cpe += ":" + version
+            else:
+                container[cpe] = int(port)
+
+        # get os cpe
+        if self.os != None:
+            for os_class in self.os["osclass"]:
+                if "cpe" in os_class and len(os_class["cpe"]) > 0:
+                    for cpe in os_class["cpe"]:
+                        container["cpe"] = -1  # -1 port indicates an OS cpe
+
+        return container
+
+    def get_vulns(self, update=True):
+        cpes = self.get_all_cpes()
+
+        vulns = self._get_os_vulns(cpes)
+        vulns.update(self._get_service_vulns(cpes))
+
+        if update:
+            self.vulns = vulns
+        return vulns
+
+    def _get_os_vulns(self, cpes):
+        if self.os == None:
+            return dict()
+        os_cpe = [cpe for (cpe, port) in cpes.items() if port == -1]
+        params = {
+            "cpeMatchString": os_cpe
+        }
+
+        cves = utils.query_nist_cve(params)
+        if cves == None:
+            return dict()
+
+        return {cve_num: vuln for (cve_num, vuln) in cves if vuln.is_vulnerable(self)}
+
+    def _get_service_vulns(self, cpes):
+        if len(self.tcp_ports) == 0:
+            return dict()
+
+        name_param = ["%s %s" % (service["product"], service["version"])
+                      for service in self.tcp_ports.values()]
+        params = {
+            "keyword": name_param
+        }
+
+        cves = utils.query_nist_cve(params)
+        if cves == None:
+            return dict()
+
+        return {cve_num: vuln for (cve_num, vuln) in cves if vuln.is_vulnerable(self)}
+
 
 class DeviceEncoder(json.JSONEncoder):
     def default(self, o):
@@ -124,7 +198,7 @@ class DeviceEncoder(json.JSONEncoder):
                     "tcp": o.tcp_ports,
                     "uptime": o.uptime,
                     "vendor": o.vendor,
-                    "vuln": o.vulnerabilities
+                    "vuln": o.vulns
                 }
             }
 
--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,8 @@ import requests
 
 from vulnerability import Vulnerability
 
-NIST_CVE_BASE_URL  = "https://services.nvd.nist.gov/rest/json/cve/1.0/"
-NIST_DB_BASE_URL   = "https://services.nvd.nist.gov/rest/json/cves/1.0"
+NIST_CVE_BASE_URL = "https://services.nvd.nist.gov/rest/json/cve/1.0"
+NIST_DB_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/1.0"
 NIST_MAX_PAGE_SIZE = 5000
 # NIST_JSON_SCHEMA = None
 # NIS_CVE_SCHEMA = None
@@ -18,16 +18,18 @@ NIST_MAX_PAGE_SIZE = 5000
 # with open(os.path.realpath("res/cve.json"), "r") as f:
 #     NIS_CVE_SCHEMA = json.load(f)
 
+
 def get_my_external_ip():
     req = requests.get('https://api.ipify.org')
     if req.status_code == requests.codes.ok:
-       return req.text
+        return req.text
     req.raise_for_status()
 
+
 def get_cve_details(cve: str):
-    req = requests.get(NIST_CVE_BASE_URL + cve)
+    req = requests.get("%s/%s" % (NIST_CVE_BASE_URL, cve))
     if req.status_code == requests.codes.ok:
-       return req.json()
+        return req.json()
     req.raise_for_status()
 
 # def matches_schema(obj, schema):
@@ -41,37 +43,41 @@ def get_cve_details(cve: str):
 #         print(e)
 #         return False
 
+
 def parse_nist_response(response: dict):
     container = dict()
 
     if response["totalResults"] == 0:
         return dict()
-    
+
     for cve_item in response["result"]["CVE_Items"]:
         cve_id = cve_item["cve"]["CVE_data_meta"]["ID"]
         container[cve_id] = Vulnerability.from_nist(cve_item)
 
     return container
 
+
 def query_nist_cve(params: dict):
-    req = requests.get(NIST_DB_BASE_URL, params = params)
+    req = requests.get(NIST_DB_BASE_URL, params=params)
     if req.status_code == requests.codes.ok:
         response = req.json()
         if len(response) == 0:
             return {}
-        
-        if response["resultsPerPage"] < response["totalResults"] and "resultsPerPage" not in response:
-            params["resultsPerPage"] = min(response["totalResults"], NIST_MAX_PAGE_SIZE)
+
+        if response["resultsPerPage"] < response["totalResults"] and "resultsPerPage" not in params:
+            params["resultsPerPage"] = min(
+                response["totalResults"], NIST_MAX_PAGE_SIZE)
             return query_nist_cve(params)
         else:
-            return parse_nist_response(response)
-    
+            return response  # parse_nist_response(response)
+
     elif req.status_code == 404:
         print("err")
         return None
-    
+
     else:
         req.raise_for_status()
+
 
 def convert_to_cpe23(cpe):
     cpe_elements = cpe.split(":")
@@ -79,6 +85,7 @@ def convert_to_cpe23(cpe):
     cpe_elements[1] = cpe_elements[1][1:]
     cpe_elements.insert(1, version)
     return ":".join(cpe_elements)
+
 
 def cpe_match(cpe_l, cpe_r):
     cpe_l_23 = convert_to_cpe23(cpe_l) if "/" in cpe_l else cpe_l
@@ -89,20 +96,26 @@ def cpe_match(cpe_l, cpe_r):
     else:
         return cpe_l_23.startswith(cpe_r_23)
 
+
 def get_version(cpe):
-    return cpe.split(":")[4] if "/" in cpe else cpe.split(":")[1]
+    details = cpe.split(":")
+    if len(details) < 5:
+        return None
+    else:
+        return cpe.split(":")[4] if "/" in cpe else cpe.split(":")[1]
+
 
 def version_compare(version_l, version_r):
     '''
     The function comapres to semantic version strings.
 
     Returns 0 if version_l and version_r represent the same version
-    
+
     Returns 1 if version_l is higher than version_r
-    
+
     Returns -1 if version_l is lower than version_r
     '''
-    
+
     ver_l = [int(x) for x in version_l.split(".")]
     ver_r = [int(x) for x in version_r.split(".")]
 
@@ -111,5 +124,47 @@ def version_compare(version_l, version_r):
             return 1
         elif ver_l[i] < ver_r[i]:
             return -1
-    
+
     return 0
+
+
+def cpe_in_list(cpe, cpe_list):
+    for item in cpe_list:
+        if cpe_match(cpe, item):
+            return True
+    return False
+
+
+def is_vulnerable(cpe_list, config_node):
+    and_node = config_node["operator"] == "AND"
+    negate = config_node["negate"] if "negate" in config_node else False
+    return_val = None
+
+    if "cpe_match" in config_node:
+        for match in config_node:
+            cpe_found = cpe_in_list(match["cpe23Uri"], cpe_list)
+            if cpe_found and not and_node:  # if cpe found and its an or node
+                return_val = True
+                break
+            elif (not cpe_found) and and_node:  # if cpe not found and its an and node
+                return_val = False
+                break
+        if return_val == None:
+            return_val = and_node  # if its an and node, return true; otherwise return false
+
+    elif "children" in config_node:
+        for child in config_node["children"]:
+            is_vuln = is_vulnerable(cpe_in_list, child)
+            if is_vuln and not and_node:
+                return_val = True
+                break
+            elif (not is_vuln) and and_node:
+                return_val = False
+                break
+        if return_val == None:
+            return_val = and_node
+
+    else:
+        raise ValueError("Node needs children or cpe_match")
+
+    return return_val if negate == False else (not return_val)
