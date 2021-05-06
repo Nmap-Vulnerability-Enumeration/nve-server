1fb3915268c81f127e4f0bd1f24b6cbc3602f7cd
samigirma1 <sgz4@students.calvin.edu>
2021-05-06 18:44:10 +0000

    Added vulns query response method

--- src/server.py
+++ src/server.py
@@ -5,25 +5,6 @@ import src.vulnerability as vulnerability
 from flask import request, jsonify, make_response
 from src.nmap_scanner import NmapScanner
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-    'title': 'A Fire Upon the Deep',
-    'author': 'Vernor Vinge',
-    'first_sentence': 'The coldsleep itself was dreamless.',
-    'year_published': '1992'},
-    {'id': 1,
-    'title': 'The Ones Who Walk Away From Omelas',
-    'author': 'Ursula K. Le Guin',
-    'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-    'published': '1973'},
-    {'id': 2,
-    'title': 'Dhalgren',
-    'author': 'Samuel R. Delany',
-    'first_sentence': 'to wound the autumnal city.',
-    'published': '1975'}
-]
-
 class NVEServer:
     def __init__(self, nmap_scanner: NmapScanner = None):
         self._scanner = nmap_scanner
@@ -36,9 +17,9 @@ class NVEServer:
         app = flask.Flask(__name__)
         app.config["DEBUG"] = True
 
-        @app.route('/', methods=['GET'])
-        def home():
-            return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+        # @app.route('/', methods=['GET'])
+        # def home():
+        #     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
         @app.route("/api/v1/setup", methods=["POST"])
         def setup_scanner():
@@ -64,7 +45,7 @@ class NVEServer:
         def api_device_all():
             try:
                 devices = self._scanner.get_all_devices()
-                return cls = device.DeviceEncoder().encode(devices)
+                return device.DeviceEncoder().encode(devices)
             except:
                 data = {'message': "Error"}
                 return make_response(jsonify(data), 500)
@@ -77,29 +58,37 @@ class NVEServer:
             if 'discovery_ip' in request.args:
                 ips = request.args['discovery_ip']
 
+                if not isinstance(ips, list):
+                    return device.DeviceEncoder().encode([self._scanner.get_device(ips)]), 200
+
+                response = []
+                for ip in ips:
+                    response.append(self._scanner.get_device(ip))
+                return device.DeviceEncoder().encode(response), 200
+            else:
+                data = {'message': "Error: No discovery_id field provided. Please specify an discovery_id."}
+                return make_response(jsonify(data), 401)
+        
+        @app.route('/api/v1/device/vuln', methods=['GET'])
+        def api_device_vuln_discovery_ip():
+            # Check if an ID was provided as part of the URL.
+            # If ID is provided, assign it to a variable.
+            # If no ID is provided, display an error in the browser.
+            if 'discovery_ip' in request.args:
+                ips = request.args['discovery_ip']
+
+                if not isinstance(ips, list):
+                    return device.DeviceEncoder().encode([self._scanner.get_device_vuln(ips)]), 200
+                
                 response = dict()
                 for ip in ips:
-                    try:
-                        val = self._scanner.get_device()
-                    except:
-                        pass
+                    response[ip] = self._scanner.get_device_vuln(ip)
+                return device.DeviceEncoder().encode(response), 200
+
             else:
                 data = {'message': "Error: No discovery_id field provided. Please specify an discovery_id."}
                 return make_response(jsonify(data), 401)
 
-            # Create an empty list for our results
-            results = []
-
-            # Loop through the data and match results that fit the requested ID.
-            # IDs are unique, but other fields might return many results
-            for book in books:
-                if book['id'] == id:
-                    results.append(book)
-
-            # Use the jsonify function from Flask to convert our list of
-            # Python dictionaries to the JSON format.
-            return jsonify(results)
-        
         @app.route('/shutdown', methods=['GET'])
         def shutdown():
             func = request.environ.get('werkzeug.server.shutdown')
